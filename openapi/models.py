0322d4a03e88f0fc3fe42aec087cdf019fdd1e20
wb-cmm753725 <wb-cmm753725@alibaba-inc.com>
2023-10-20 15:52:32 +0800

    generate code and generate_data_key_pair apis

--- openapi/models.py
+++ openapi/models.py
@@ -1,59 +1,85 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
+# This file is auto-generated, don't edit it. Thanks.
 from Tea.model import TeaModel
+
+from openapi_credential.client import Client as DedicatedKmsOpenapiCredentialClient
 
 
 class Config(TeaModel):
     def __init__(
             self,
-            access_key_id=None,
-            private_key=None,
-            endpoint=None,
-            protocol=None,
-            region_id=None,
-            read_timeout=None,
-            connect_timeout=None,
-            http_proxy=None,
-            https_proxy=None,
-            socks_5proxy=None,
-            socks_5net_work=None,
-            no_proxy=None,
-            max_idle_conns=None,
-            user_agent=None,
-            type=None,
-            credential=None,
-            client_key_file=None,
-            client_key_content=None,
-            password=None,
+            access_key_id: str = None,
+            private_key: str = None,
+            endpoint: str = None,
+            protocol: str = None,
+            region_id: str = None,
+            read_timeout: int = None,
+            connect_timeout: int = None,
+            http_proxy: str = None,
+            https_proxy: str = None,
+            no_proxy: str = None,
+            max_idle_conns: int = None,
+            socks_5proxy: str = None,
+            socks_5net_work: str = None,
+            type: str = None,
+            user_agent: str = None,
+            credential: DedicatedKmsOpenapiCredentialClient = None,
+            client_key_file: str = None,
+            client_key_content: str = None,
+            password: str = None,
+            ca_file_path: str = None,
+            ignore_ssl: bool = None,
     ):
+        # 访问凭证ID
         self.access_key_id = access_key_id
-        # pkcs1 or pkcs8 PEM format private key
+        # pkcs1 或 pkcs8 PEM 格式私钥
         self.private_key = private_key
-        # crypto service address
+        # 实例地址
         self.endpoint = endpoint
+        # 协议
         self.protocol = protocol
+        # 区域标识
         self.region_id = region_id
+        # 读取超时时间
         self.read_timeout = read_timeout
+        # 连接超时时间
         self.connect_timeout = connect_timeout
+        # http代理
         self.http_proxy = http_proxy
+        # https代理
         self.https_proxy = https_proxy
-        self.socks_5proxy = socks_5proxy
-        self.socks_5net_work = socks_5net_work
+        # 无代理
         self.no_proxy = no_proxy
+        # 最大闲置连接数
         self.max_idle_conns = max_idle_conns
-        self.user_agent = user_agent
+        # socks5代理
+        self.socks_5proxy = socks_5proxy
+        # socks5代理协议
+        self.socks_5net_work = socks_5net_work
+        # 访问凭证类型
         self.type = type
+        # 用户代理
+        self.user_agent = user_agent
+        # 访问凭证
         self.credential = credential
+        # ClientKey文件路径
         self.client_key_file = client_key_file
-        # client key content
+        # ClientKey文件内容
         self.client_key_content = client_key_content
+        # ClientKey密码
         self.password = password
+        # ca证书文件路径
+        self.ca_file_path = ca_file_path
+        # 是否忽略SSL认证
+        self.ignore_ssl = ignore_ssl
 
     def validate(self):
         if self.region_id is not None:
             self.validate_pattern(self.region_id, 'region_id', '[a-zA-Z0-9-_]+')
+        self.validate_required(self.type, 'type')
 
     def to_map(self):
-        _map = super(Config, self).to_map()
+        _map = super().to_map()
         if _map is not None:
             return _map
 
@@ -76,18 +102,18 @@ class Config(TeaModel):
             result['httpProxy'] = self.http_proxy
         if self.https_proxy is not None:
             result['httpsProxy'] = self.https_proxy
-        if self.socks_5proxy is not None:
-            result['socks5Proxy'] = self.socks_5proxy
-        if self.socks_5net_work is not None:
-            result['socks5NetWork'] = self.socks_5net_work
         if self.no_proxy is not None:
             result['noProxy'] = self.no_proxy
         if self.max_idle_conns is not None:
             result['maxIdleConns'] = self.max_idle_conns
-        if self.user_agent is not None:
-            result['userAgent'] = self.user_agent
+        if self.socks_5proxy is not None:
+            result['socks5Proxy'] = self.socks_5proxy
+        if self.socks_5net_work is not None:
+            result['socks5NetWork'] = self.socks_5net_work
         if self.type is not None:
             result['type'] = self.type
+        if self.user_agent is not None:
+            result['userAgent'] = self.user_agent
         if self.credential is not None:
             result['credential'] = self.credential
         if self.client_key_file is not None:
@@ -96,9 +122,13 @@ class Config(TeaModel):
             result['clientKeyContent'] = self.client_key_content
         if self.password is not None:
             result['password'] = self.password
+        if self.ca_file_path is not None:
+            result['caFilePath'] = self.ca_file_path
+        if self.ignore_ssl is not None:
+            result['ignoreSSL'] = self.ignore_ssl
         return result
 
-    def from_map(self, m=None):
+    def from_map(self, m: dict = None):
         m = m or dict()
         if m.get('accessKeyId') is not None:
             self.access_key_id = m.get('accessKeyId')
@@ -118,18 +148,18 @@ class Config(TeaModel):
             self.http_proxy = m.get('httpProxy')
         if m.get('httpsProxy') is not None:
             self.https_proxy = m.get('httpsProxy')
-        if m.get('socks5Proxy') is not None:
-            self.socks_5proxy = m.get('socks5Proxy')
-        if m.get('socks5NetWork') is not None:
-            self.socks_5net_work = m.get('socks5NetWork')
         if m.get('noProxy') is not None:
             self.no_proxy = m.get('noProxy')
         if m.get('maxIdleConns') is not None:
             self.max_idle_conns = m.get('maxIdleConns')
-        if m.get('userAgent') is not None:
-            self.user_agent = m.get('userAgent')
+        if m.get('socks5Proxy') is not None:
+            self.socks_5proxy = m.get('socks5Proxy')
+        if m.get('socks5NetWork') is not None:
+            self.socks_5net_work = m.get('socks5NetWork')
         if m.get('type') is not None:
             self.type = m.get('type')
+        if m.get('userAgent') is not None:
+            self.user_agent = m.get('userAgent')
         if m.get('credential') is not None:
             self.credential = m.get('credential')
         if m.get('clientKeyFile') is not None:
@@ -138,11 +168,8 @@ class Config(TeaModel):
             self.client_key_content = m.get('clientKeyContent')
         if m.get('password') is not None:
             self.password = m.get('password')
+        if m.get('caFilePath') is not None:
+            self.ca_file_path = m.get('caFilePath')
+        if m.get('ignoreSSL') is not None:
+            self.ignore_ssl = m.get('ignoreSSL')
         return self
-
-
-class ResponseEntity(object):
-
-    def __init__(self, body_bytes, response_headers):
-        self.body_bytes = body_bytes
-        self.response_headers = response_headers
--- openapi_credential/models.py
+++ openapi_credential/models.py
@@ -1,4 +1,4 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
 # This file is auto-generated, don't edit it. Thanks.
 from Tea.model import TeaModel
 
@@ -6,88 +6,96 @@ from Tea.model import TeaModel
 class Config(TeaModel):
     def __init__(
             self,
-            type=None,
-            access_key_id=None,
-            private_key=None,
-            client_key_file=None,
-            client_key_content=None,
-            password=None,
+            type: str = None,
+            access_key_id: str = None,
+            private_key: str = None,
+            client_key_file: str = None,
+            client_key_content: str = None,
+            password: str = None,
     ):
+        # 访问凭证类型
         self.type = type
+        # 访问凭证ID
         self.access_key_id = access_key_id
+        # pkcs1 或 pkcs8 PEM 格式私钥
         self.private_key = private_key
+        # ClientKey文件路径
         self.client_key_file = client_key_file
+        # ClientKey文件内容
         self.client_key_content = client_key_content
+        # ClientKey密码
         self.password = password
 
     def validate(self):
         self.validate_required(self.type, 'type')
 
     def to_map(self):
-        _map = super(Config, self).to_map()
+        _map = super().to_map()
         if _map is not None:
             return _map
 
         result = dict()
         if self.type is not None:
-            result['type'] = self.type
+            result['Type'] = self.type
         if self.access_key_id is not None:
-            result['accessKeyId'] = self.access_key_id
+            result['Type'] = self.access_key_id
         if self.private_key is not None:
-            result['privateKey'] = self.private_key
+            result['Type'] = self.private_key
         if self.client_key_file is not None:
-            result['clientKeyFile'] = self.client_key_file
+            result['Type'] = self.client_key_file
         if self.client_key_content is not None:
-            result['clientKeyContent'] = self.client_key_content
+            result['Type'] = self.client_key_content
         if self.password is not None:
-            result['password'] = self.password
+            result['Type'] = self.password
         return result
 
-    def from_map(self, m=None):
+    def from_map(self, m: dict = None):
         m = m or dict()
-        if m.get('type') is not None:
-            self.type = m.get('type')
-        if m.get('accessKeyId') is not None:
-            self.access_key_id = m.get('accessKeyId')
-        if m.get('privateKey') is not None:
-            self.private_key = m.get('privateKey')
-        if m.get('clientKeyFile') is not None:
-            self.client_key_file = m.get('clientKeyFile')
-        if m.get('clientKeyContent') is not None:
-            self.client_key_content = m.get('clientKeyContent')
-        if m.get('password') is not None:
-            self.password = m.get('password')
+        if m.get('Type') is not None:
+            self.type = m.get('Type')
+        if m.get('Type') is not None:
+            self.access_key_id = m.get('Type')
+        if m.get('Type') is not None:
+            self.private_key = m.get('Type')
+        if m.get('Type') is not None:
+            self.client_key_file = m.get('Type')
+        if m.get('Type') is not None:
+            self.client_key_content = m.get('Type')
+        if m.get('Type') is not None:
+            self.password = m.get('Type')
         return self
 
 
-class ClientKey(TeaModel):
+class RsaKeyPairCredentials(TeaModel):
     def __init__(
             self,
-            key_id=None,
-            private_key_data=None,
+            private_key_secret: str = None,
+            key_id: str = None,
     ):
+        # 访问凭证私钥
+        self.private_key_secret = private_key_secret
+        # 访问凭证ID
         self.key_id = key_id
-        self.private_key_data = private_key_data
 
     def validate(self):
         pass
 
     def to_map(self):
-        _map = super(ClientKey, self).to_map()
+        _map = super().to_map()
         if _map is not None:
             return _map
 
         result = dict()
+        if self.private_key_secret is not None:
+            result['privateKeySecret'] = self.private_key_secret
         if self.key_id is not None:
-            result['KeyId'] = self.key_id
-        if self.private_key_data is not None:
-            result['PrivateKeyData'] = self.private_key_data
+            result['keyId'] = self.key_id
         return result
 
-    def from_map(self, m=None):
+    def from_map(self, m: dict = None):
         m = m or dict()
-        if m.get('KeyId') is not None:
-            self.key_id = m.get('KeyId')
-        if m.get('PrivateKeyData') is not None:
-            self.private_key_data = m.get('PrivateKeyData')
+        if m.get('privateKeySecret') is not None:
+            self.private_key_secret = m.get('privateKeySecret')
+        if m.get('keyId') is not None:
+            self.key_id = m.get('keyId')
         return self
